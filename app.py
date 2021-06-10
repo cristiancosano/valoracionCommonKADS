da5ff47a4ce915a0357ebce0c16c7d3e1986b449
cristiancosano <cristiancosano@icloud.com>
2021-06-10 17:52:31 +0200

    renombrado

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-Nombre: ctkCtrlValoracion.py
-Descripcion: Base del Conocimiento De Valoración
+Nombre: app.py
+Descripcion: Aplicacion
 Asignatura: Ingeniería de Sistemas Software Basados en Conocimiento
 Autor: Cristian Cosano Cejas y Antonio Luis Rodriguez Jiménez 
 Fecha: 20/06/2021
--- ctkCtrlValoracion.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-Nombre: ctkCtrlValoracion.py
-Descripcion: Base del Conocimiento De Valoración
-Asignatura: Ingeniería de Sistemas Software Basados en Conocimiento
-Autor: Cristian Cosano Cejas y Antonio Luis Rodriguez Jiménez 
-Fecha: 20/06/2021
-'''
