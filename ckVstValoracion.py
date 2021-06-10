4ef261fef123e2aa013152851ab894dd0a96826d
cristiancosano <cristiancosano@icloud.com>
2021-06-10 17:41:44 +0200

    Añadidos ficheros

---
+++ ckVstValoracion.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Nombre: ckVstValoracion.py
+Descripcion: Base del Conocimiento De Valoración
+Asignatura: Ingeniería de Sistemas Software Basados en Conocimiento
+Autor: Cristian Cosano Cejas y Antonio Luis Rodriguez Jiménez 
+Fecha: 20/06/2021
+'''
